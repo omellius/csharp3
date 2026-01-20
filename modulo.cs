24f153805c6ae974be51ed9eae41d4cd1acc01a2
omellius <olivermg@student.uia.no>
2026-01-20 20:56:46 +0100

    Add modulo check for even or odd numbers

---
+++ modulo.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Skriv in et heltall: ");
+int tall = int.Parse(Console.Readline());
+
+if (tall % 2 == 0)
+{
+    Console.WriteLine("Tallet er partall.");
+}
+else
+{
+    Console.WriteLine("Tallet er oddetall.");
+}
